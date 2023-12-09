ac973ea0db731e3eeb687daa73a05d1ad5dc5d6c
JaeJun <yyggb12@kakao.com>
2023-12-09 14:19:38 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for(int i=0; i<code.length(); i++) {
+            switch(mode) {
+                case 0:
+                    if(code.charAt(i) != '1') {
+                        if(i % 2 == 0) {
+                            answer += "" + code.charAt(i);
+                        }
+                    } else {
+                        mode = 1;
+                    }
+                    break;      
+                case 1:
+                    if(code.charAt(i) != '1') {
+                        if(i % 2 == 1) {
+                            answer += "" + code.charAt(i);
+                        }
+                    } else {
+                        mode = 0;
+                    }
+                    break;
+            }
+        }
+    
+        if(answer.length() == 0) {
+            return "EMPTY";
+        }
+        
+        return answer;
+    }
+}
