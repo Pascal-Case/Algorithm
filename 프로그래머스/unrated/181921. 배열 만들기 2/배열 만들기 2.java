6bc144a11dce501a409abac24fe59adce0e82deb
JaeJun <yyggb12@kakao.com>
2023-12-21 17:33:36 +0900

    [level unrated] Title: 배열 만들기 2, Time: 8.20 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int l, int r) {
+        int[] answer = IntStream.range(l, r + 1)
+                .filter(x -> (Integer.toString(x).startsWith("5")))
+                .filter(x -> (Integer.toString(x).endsWith("5") || Integer.toString(x).endsWith("0")))
+                .filter(x -> {
+                    if (x == 5) {
+                        return true;
+                    }
+
+                    while (x / 10 != 0) {
+                        if (Integer.toString(x).endsWith("5") || Integer.toString(x).endsWith("0")) {
+                            x /= 10;
+                        } else {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                })
+                .toArray();
+
+        return answer.length > 0 ? answer : new int[]{-1};
+    }
+}
