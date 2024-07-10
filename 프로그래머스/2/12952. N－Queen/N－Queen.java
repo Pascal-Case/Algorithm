07a3ed4101807aa30087c426a249ca0eaea09909
JaeJun <yyggb12@kakao.com>
2024-07-10 22:55:30 +0900

    [level 2] Title: N-Queen, Time: 194.10 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,41 +1,41 @@
 class Solution {
     private static int[] board;
-    private static int answer = 0;
+    private static int cnt = 0;
     
     public int solution(int n) {
         board = new int[n];
         
         backTracking(0, n);
         
-        return answer;
+        return cnt;
     }
     
-    public void backTracking(int depth, int n) {
-        if(depth == n) {
-            answer ++;
+    public void backTracking(int row, int n) {
+        if(row == n) {
+            cnt++;
             return;
         }
         
         for(int i = 0; i < n; i++) {
-            board[depth] = i;
-            if(possible(depth)) {
-                backTracking(depth + 1, n);
+            board[row] = i;
+            
+            if(isPromising(row)) {
+                backTracking(row + 1, n);
             }
         }
     }
     
-    public boolean possible(int depth) {
+    public boolean isPromising(int row) {
         
-        for(int i = 0; i < depth; i++) {
-            if(board[i] == board[depth]) {
-                return false;
-            }
+        for(int i = 0; i < row; i++) {
             
-            if(Math.abs(depth - i) == Math.abs(board[depth] - board[i])) {
-                return false;
-            }
+            if(board[i] == board[row]) return false;
+            
+            if(Math.abs(row - i) == Math.abs(board[row] - board[i])) return false;
+            
         }
         return true;
     }
+    
     
 }
