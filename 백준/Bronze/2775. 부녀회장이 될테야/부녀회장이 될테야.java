35de8d0fa2333d0a782a592879835fb0982887e3
JaeJun <yyggb12@kakao.com>
2024-07-16 13:40:54 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            sb.append(solution(k, n)).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+    public static int solution(int k, int n) {
+        int answer = 0;
+        int[][] dp = new int[k + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                int sum = 0;
+                for (int l = 1; l <= j; l++) {
+                    sum += dp[i - 1][l];
+                }
+                dp[i][j] = sum;
+            }
+        }
+
+        return dp[k][n];
+    }
+}
+
