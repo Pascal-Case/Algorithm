bec31b00aa4f259db32747e15e15f7546b30c61b
JaeJun <yyggb12@kakao.com>
2024-07-21 17:02:31 +0900

    [level 0] Title: 배열의 유사도, Time: 2.93 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        return (int) Arrays.stream(s1).filter(s -> Arrays.stream(s2).anyMatch(ss -> ss.equals(s))).count();
+    }
+}
