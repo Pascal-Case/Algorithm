c3c96cd46a4561b0ec1d372ddd4919ff1402395b
JaeJun <yyggb12@kakao.com>
2024-07-23 00:00:00 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,9 +1,6 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int cnt = 0;
-        for(int n : num_list) {
-            if(n % 2 == 0) cnt++;
-        }
-        return new int[] {cnt, num_list.length - cnt};
+        int[] answer = {};
+        return answer;
     }
 }
