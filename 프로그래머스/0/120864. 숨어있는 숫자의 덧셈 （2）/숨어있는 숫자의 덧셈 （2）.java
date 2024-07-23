c7e9c5f56de5c8310ceaa90dfe9ba949b4342e9c
JaeJun <yyggb12@kakao.com>
2024-07-23 20:00:38 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 3.94 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(String my_string) {
+        return Arrays.stream(my_string.replaceAll("[a-zA-Z]", "!").split("!")).filter(s -> !s.isEmpty()).mapToInt(i -> Integer.parseInt(i)).sum();
+    }
+}
