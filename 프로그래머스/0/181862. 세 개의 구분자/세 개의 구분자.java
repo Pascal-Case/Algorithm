a709519f5dfe86528cf3aae8bbbed661519a38c6
JaeJun <yyggb12@kakao.com>
2024-07-23 22:36:04 +0900

    [level 0] Title: 세 개의 구분자, Time: 73.89 ms, Memory: 98.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String myStr) {
+        String[] answer = Arrays.stream(myStr.split("[abc]")).filter(s -> !s.isEmpty()).toArray(String[]::new);
+        return answer.length == 0 ? new String[]{"EMPTY"} : answer;
+    }
+}
