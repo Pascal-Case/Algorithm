f088ab147ee5e8c1d8e8b40f98ac0a2b2c924245
JAEJUN YANG <yyggb12@kakao.com>
2025-01-01 22:04:56 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 47.03 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        boolean flag = true;
+        for(int i = 0; i < n; i++) {
+            if(flag) {
+                answer += "수";
+                flag = false;
+            } else {
+                answer += "박";
+                flag = true;
+            }
+        }
+        return answer;
+    }
+}
