76e72664a5dacdc629c8771960ce48fa0a48fcbe
JAEJUN YANG <yyggb12@kakao.com>
2025-01-02 22:51:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,6 +1,11 @@
 class Solution {
     public String solution(String phone_number) {
-        int len = phone_number.length();
-        return "*".repeat(len-4) + phone_number.substring(len-4);
+        char[] ch = phone_number.toCharArray();
+        
+        for(int i = 0; i < ch.length - 4; i++) {
+            ch[i] = '*';
+        }
+        
+        return String.valueOf(ch);
     }
 }
