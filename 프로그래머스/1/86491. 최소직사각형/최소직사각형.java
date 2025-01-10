5b7f5873bf47f70c4fe1cc3099131b725e47376c
JAEJUN YANG <yyggb12@kakao.com>
2025-01-10 11:03:18 +0900

    [level 1] Title: 최소직사각형, Time: 1.60 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int w = Integer.MIN_VALUE;
+        int h = Integer.MIN_VALUE;
+        
+        for(int[] size : sizes) {
+            w = Math.max(w, Math.max(size[0], size[1]));
+            h = Math.max(h, Math.min(size[0], size[1]));
+        }
+        
+        return w * h;
+    }
+}
