ff9facf8fbbe2d966627f61e626e67277f63308a
JAEJUN YANG <yyggb12@kakao.com>
2025-02-11 13:23:14 +0900

    [level 1] Title: 바탕화면 정리, Time: 3.31 ms, Memory: 94.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < wallpaper.length; i++) {
+            String[] wall = wallpaper[i].split("");
+            for(int j = 0; j < wall.length; j++) {
+                if(wall[j].equals("#")) {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i + 1);
+                    rdy = Math.max(rdy, j + 1);
+                }
+            }
+        }
+        
+        return new int []{lux, luy, rdx, rdy};
+    }
+}
