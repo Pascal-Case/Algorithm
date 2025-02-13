1e4565f3230c160fd4252959e29e00c8f331613c
JAEJUN YANG <yyggb12@kakao.com>
2025-02-13 12:49:31 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.23 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,47 +1,40 @@
 import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answerInt = new ArrayList<>();
-        int[] todayInt = new int[3];
-        int[] date = new int[3];
-        String term = "";
+        List<Integer> list = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
-        todayInt[0] =  Integer.valueOf(today.substring(0, 4));
-        todayInt[1] =  Integer.valueOf(today.substring(5, 7));
-        todayInt[2] =  Integer.valueOf(today.substring(8, 10));
+        for(String t : terms) {
+            map.put(t.split(" ")[0], Integer.parseInt(t.split(" ")[1]));
+        }
+        
+        int y = Integer.parseInt(today.substring(0, 4));
+        int m = Integer.parseInt(today.substring(5, 7));
+        int d = Integer.parseInt(today.substring(8, 10));
+        int todayTotal = d + (m * 28) + (y * 28 * 12);
         
         for(int i = 0; i < privacies.length; i++) {
-            date[0] =  Integer.valueOf(privacies[i].substring(0, 4));
-            date[1] =  Integer.valueOf(privacies[i].substring(5, 7));
-            date[2] =  Integer.valueOf(privacies[i].substring(8, 10));
-            term += privacies[i].charAt(11);
-            for(int j = 0; j < terms.length; j++) {
-                if(term.equals(terms[j].charAt(0) + "")) {
-                    date[1] += Integer.valueOf(terms[j].substring(2, terms[j].length()));
-                    if(date[1] > 12) {
-                        date[0] += date[1] / 12;
-                        date[1] = date[1] % 12;
-                    } 
-                    if(date[1] == 0) {
-                        date[0]--;
-                        date[1] = 12;
-                    }
-                    
-                    if(date[0] == todayInt[0]) {
-                        if(date[1] == todayInt[1]) {
-                            if(date[2] <= todayInt[2]) answerInt.add(i+1);
-                        } else if(date[1] < todayInt[1]) answerInt.add(i+1);
-                    } else if(date[0] < todayInt[0]) answerInt.add(i+1);
-                }
-            }
-            term = "";
+            String privacy = privacies[i];
+            String term = privacy.split(" ")[1];         
+                        
+            String date = privacy.split(" ")[0];
+            int l = map.get(term);
+            
+            int limitY = Integer.parseInt(date.substring(0, 4));
+            int limitM = Integer.parseInt(date.substring(5, 7));
+            int limitD = Integer.parseInt(date.substring(8, 10));
+
+            int limitTotal = limitD + (limitM * 28) + (l * 28) + (limitY * 28 * 12);
+            
+            if(todayTotal >= limitTotal) list.add(i+1);
+            
         }
         
-        int[] answer = new int[answerInt.size()];
-        for(int i = 0; i < answerInt.size(); i++) {
-            answer[i] = answerInt.get(i);
-        }
         
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
